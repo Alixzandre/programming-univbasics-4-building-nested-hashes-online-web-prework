e0a75d00d130d350ea5fbb70a5bcdb936b87215c
Alexander Bispo <alexbispo173@gmail.com>
2019-09-02 00:10:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,7 @@ def monopoly_with_fourth_tier
         four_pieces_owned: 200
       },
     names: {
-      reading_railroad: {},
+      reading_railroad: {"mor"},
       pennsylvania_railroad: {},
       b_and_o_railroad: {},
       shortline_railroad: {}
